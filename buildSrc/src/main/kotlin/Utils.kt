5a12f0e2eb91a74fee4d2b3746245b0d0bbc396b
junsuPark <krpjs0508@gmail.com>
2023-07-24 15:55:32 +0900

    ♻️ :: import 수정

--- buildSrc/src/main/kotlin/Utils.kt
+++ buildSrc/src/main/kotlin/Utils.kt
@@ -1,19 +1,22 @@
-fun org.gradle.api.artifacts.dsl.DependencyHandler.implementation(vararg dependencies: String) {
+import org.gradle.api.artifacts.dsl.DependencyHandler
+import org.gradle.plugin.use.PluginDependenciesSpec
+
+fun DependencyHandler.implementation(vararg dependencies: String) {
     dependencies.forEach { dependency -> add("implementation", dependency) }
 }
 
-fun org.gradle.api.artifacts.dsl.DependencyHandler.androidTestImplementation(vararg dependencies: String) {
+fun DependencyHandler.androidTestImplementation(vararg dependencies: String) {
     dependencies.forEach { dependency -> add("androidTestImplementation", dependency) }
 }
 
-fun org.gradle.api.artifacts.dsl.DependencyHandler.testImplementation(vararg dependencies: String) {
+fun DependencyHandler.testImplementation(vararg dependencies: String) {
     dependencies.forEach { dependency -> add("testImplementation", dependency) }
 }
 
-fun org.gradle.api.artifacts.dsl.DependencyHandler.kapt(vararg dependencies: String) {
+fun DependencyHandler.kapt(vararg dependencies: String) {
     dependencies.forEach { dependency -> add("kapt", dependency) }
 }
 
-fun org.gradle.plugin.use.PluginDependenciesSpec.id(vararg ids: String) {
+fun PluginDependenciesSpec.id(vararg ids: String) {
     ids.forEach { id -> this.id(id) }
 }
