97554bc52da02b81c1e22e44b8b697c47c14bd03
junsuPark <krpjs0508@gmail.com>
2023-07-25 09:56:07 +0900

    📑 :: MainActivity, MainActivityViewModel 구현

--- app/src/main/kotlin/com/xquare/androidv2/MainActivity.kt
+++ app/src/main/kotlin/com/xquare/androidv2/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.xquare.androidv2
 
 import androidx.appcompat.app.AppCompatActivity
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MainActivity : AppCompatActivity()
+class MainActivity : AppCompatActivity() {
+    private val viewModel: MainActivityViewModel by viewModel()
+}
