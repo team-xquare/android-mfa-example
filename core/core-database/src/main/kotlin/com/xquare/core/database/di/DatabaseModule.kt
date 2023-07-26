d55631ec7a4135f1d20a66995dcb5501d1b1500a
junsuPark <krpjs0508@gmail.com>
2023-07-26 11:27:45 +0900

    ⚡️ :: database 내부 di 로직 추가

--- core/core-database/src/main/kotlin/com/xquare/core/database/di/DatabaseModule.kt
+++ core/core-database/src/main/kotlin/com/xquare/core/database/di/DatabaseModule.kt
@@ -2,10 +2,16 @@ package com.xquare.core.database.di
 
 import androidx.room.Room
 import com.xquare.core.database.XquareDatabase
+import com.xquare.core.database.datasource.MealDatasource
+import com.xquare.core.database.datasource.MealDatasourceImpl
 import org.koin.android.ext.koin.androidApplication
 import org.koin.dsl.module
 
 val databaseModule = module {
+    includes(
+        datasourceModule,
+        daoModule,
+    )
     single {
         Room.databaseBuilder(
             context = androidApplication(),
@@ -13,4 +19,12 @@ val databaseModule = module {
             name = "database-xquare",
         ).build()
     }
+}
+
+private val datasourceModule = module {
+    single<MealDatasource> { MealDatasourceImpl(get()) }
+}
+
+private val daoModule = module {
+    single { get<XquareDatabase>().mealDao() }
 }
