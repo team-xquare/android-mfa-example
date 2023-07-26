c84e071b93d9157a1adfee357af8563b450124df
junsuPark <krpjs0508@gmail.com>
2023-07-26 11:18:39 +0900

    📑 :: MealDatasource 구현

---
+++ core/core-database/src/main/kotlin/com/xquare/core/database/datasource/MealDatasource.kt
@@ -0,0 +1,25 @@
+package com.xquare.core.database.datasource
+
+import com.xquare.core.model.Meal
+import kotlinx.coroutines.flow.Flow
+import kotlinx.datetime.Instant
+
+interface MealDatasource {
+
+    fun fetchMeal(date: Instant): Flow<Meal>
+
+    fun fetchMealsWithTerm(
+        from: Instant,
+        to: Instant,
+    ): Flow<List<Meal>>
+
+    fun fetchAllMeals(): Flow<List<Meal>>
+
+    fun saveMeal(meal: Meal)
+
+    fun saveAllMeals(meal: List<Meal>)
+
+    fun deleteMealByDate(date: Instant)
+
+    fun deleteAllMeals()
+}
