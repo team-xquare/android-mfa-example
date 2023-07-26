fad165e02ad53a7d32f0a86d9f35e7defbbf7de1
junsuPark <krpjs0508@gmail.com>
2023-07-26 11:23:38 +0900

    ⚡️ :: database di 구현

--- core/core-database/src/main/kotlin/com/xquare/core/database/di/DatabaseModule.kt
+++ core/core-database/src/main/kotlin/com/xquare/core/database/di/DatabaseModule.kt
@@ -1,5 +1,16 @@
 package com.xquare.core.database.di
 
+import androidx.room.Room
+import com.xquare.core.database.XquareDatabase
+import org.koin.android.ext.koin.androidApplication
 import org.koin.dsl.module
 
-val databaseModule = module {}
+val databaseModule = module {
+    single {
+        Room.databaseBuilder(
+            context = androidApplication(),
+            klass = XquareDatabase::class.java,
+            name = "database-xquare",
+        ).build()
+    }
+}
