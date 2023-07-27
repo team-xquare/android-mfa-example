d3d78d3458bd2a12ad7073a4e8ae9a2fe4579386
junsuPark <krpjs0508@gmail.com>
2023-07-27 15:53:02 +0900

    📑 :: Di Qualifier 생성

---
+++ core/core-common/src/main/kotlin/com/xquare/core/common/di/DiQualifier.kt
@@ -0,0 +1,10 @@
+package com.xquare.core.common.di
+
+object DiQualifier {
+    object Authorization {
+        const val ACCESS_TOKEN = "access_token"
+        const val ACCESS_TOKEN_EXPIRATION = "access_token_expiration"
+        const val REFRESH_TOKEN = "refresh_token"
+        const val REFRESH_TOKEN_EXPIRATION = "refresh_token_expiration"
+    }
+}
