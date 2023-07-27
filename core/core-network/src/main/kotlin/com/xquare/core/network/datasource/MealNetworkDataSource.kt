2d38dfbec26339db751c88fd3ca88d56a51da4dc
junsuPark <krpjs0508@gmail.com>
2023-07-27 13:25:30 +0900

    ♻️ :: network datasource 구현 수정

--- core/core-network/src/main/kotlin/com/xquare/core/network/datasource/MealNetworkDataSource.kt
+++ core/core-network/src/main/kotlin/com/xquare/core/network/datasource/MealNetworkDataSource.kt
@@ -1,3 +1,9 @@
 package com.xquare.core.network.datasource
 
-interface MealNetworkDataSource
+import com.xquare.core.model.Meal
+import kotlinx.datetime.LocalDate
+
+interface MealNetworkDataSource {
+    suspend fun fetchMeal(date: LocalDate): Meal
+    suspend fun fetchMealOfMonth(date: LocalDate): List<Meal>
+}
