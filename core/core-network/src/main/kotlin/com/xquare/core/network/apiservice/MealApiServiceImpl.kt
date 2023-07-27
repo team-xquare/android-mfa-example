06f42c461343cd48a02c669e5283436b52e9bdc7
junsuPark <krpjs0508@gmail.com>
2023-07-27 17:03:39 +0900

    ⚡️ :: 급식 조회 구현

--- core/core-network/src/main/kotlin/com/xquare/core/network/apiservice/MealApiServiceImpl.kt
+++ core/core-network/src/main/kotlin/com/xquare/core/network/apiservice/MealApiServiceImpl.kt
@@ -2,16 +2,24 @@ package com.xquare.core.network.apiservice
 
 import com.xquare.core.network.model.MealResponse
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
 import kotlinx.datetime.LocalDate
 
 internal class MealApiServiceImpl(
     private val httpClient: HttpClient,
 ) : MealApiService {
-    override suspend fun fetchMeal(date: LocalDate): MealResponse {
-        TODO("Not yet implemented")
-    }
+    override suspend fun fetchMeal(date: LocalDate): MealResponse = httpClient.get("/meals") {
+        url { parameters.append("date", date.toString()) }
+    }.body()
 
-    override suspend fun fetchMealOfMonth(date: LocalDate): List<MealResponse> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun fetchMealOfMonth(date: LocalDate): List<MealResponse> =
+        httpClient.get("/meals") {
+            url {
+                parameters.run {
+                    append("year", date.year.toString())
+                    append("month", date.monthNumber.toString())
+                }
+            }
+        }.body()
 }
