9febba200523dda9db13eaba61b38dad9b5bba30
junsuPark <krpjs0508@gmail.com>
2023-07-27 10:45:19 +0900

    ♻️ :: instant -> localdate 파라미터 수정

--- core/core-database/src/main/kotlin/com/xquare/core/database/datasource/MealDatasource.kt
+++ core/core-database/src/main/kotlin/com/xquare/core/database/datasource/MealDatasource.kt
@@ -2,14 +2,14 @@ package com.xquare.core.database.datasource
 
 import com.xquare.core.model.Meal
 import kotlinx.coroutines.flow.Flow
-import kotlinx.datetime.Instant
+import kotlinx.datetime.LocalDate
 
 interface MealDatasource {
-    fun fetchMeal(date: Instant): Flow<Meal>
-    fun fetchMealsWithTerm(from: Instant, to: Instant): Flow<List<Meal>>
+    fun fetchMeal(date: LocalDate): Flow<Meal>
+    fun fetchMealsWithTerm(from: LocalDate, to: LocalDate): Flow<List<Meal>>
     fun fetchAllMeals(): Flow<List<Meal>>
     fun saveMeal(meal: Meal)
     fun saveAllMeals(meal: List<Meal>)
-    fun deleteMealByDate(date: Instant)
+    fun deleteMealByDate(date: LocalDate)
     fun deleteAllMeals()
 }
