79e346318fc9dbdbd77f1112b66d0424d5bee362
junsuPark <krpjs0508@gmail.com>
2023-07-27 16:19:26 +0900

    ⚡️ :: debug에 따른 URL 제공 로직 구현

--- core/core-network/src/main/kotlin/com/xquare/core/network/client/HttpClient.kt
+++ core/core-network/src/main/kotlin/com/xquare/core/network/client/HttpClient.kt
@@ -1,9 +1,10 @@
 package com.xquare.core.network.client
 
-import com.xquare.core.network.BuildConfig
+import com.xquare.core.common.util.isDebugEnabled
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.ANDROID
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
@@ -15,11 +16,23 @@ internal val httpClient = HttpClient(CIO) {
     expectSuccess = true
 
     install(ContentNegotiation) { json() }
-    if (BuildConfig.DEBUG) {
+    if (isDebugEnabled) {
         install(Logging) {
             logger = Logger.ANDROID
             level = LogLevel.BODY
             sanitizeHeader { header -> header == HttpHeaders.Authorization }
         }
     }
+    defaultRequest {
+        url(
+            urlString = if (isDebugEnabled) {
+                STAG_BASE_URL
+            } else {
+                PROD_BASE_URL
+            },
+        )
+    }
 }
+
+private const val PROD_BASE_URL = "https://api.xquare.app"
+private const val STAG_BASE_URL = "https://stag-api.xquare.app"
