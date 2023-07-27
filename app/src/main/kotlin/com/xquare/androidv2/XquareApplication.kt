52ab1f38a6b99cbdec20091ae3e9ee509c6609db
junsuPark <krpjs0508@gmail.com>
2023-07-27 18:00:42 +0900

    ♻️ :: Application 리팩토링

--- app/src/main/kotlin/com/xquare/androidv2/XquareApplication.kt
+++ app/src/main/kotlin/com/xquare/androidv2/XquareApplication.kt
@@ -2,30 +2,24 @@ package com.xquare.androidv2
 
 import android.app.Application
 import com.xquare.androidv2.di.xquareModule
+import com.xquare.core.common.util.isDebugEnabled
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
 class XquareApplication : Application() {
-
-    // todo 적절한 모듈로 이동
-    private val isDebugEnabled: Boolean
-        get() = BuildConfig.DEBUG
-
     override fun onCreate() {
         super.onCreate()
-
         initKoin()
     }
 
     private fun initKoin() {
         startKoin {
+            modules(xquareModule)
+            androidContext(this@XquareApplication)
             if (isDebugEnabled) {
                 androidLogger()
             }
-
-            androidContext(this@XquareApplication)
-            modules(xquareModule)
         }
     }
 }
