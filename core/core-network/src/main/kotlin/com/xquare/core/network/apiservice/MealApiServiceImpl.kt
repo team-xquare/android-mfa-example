d57ffab51f89e250dbbb66b069127449a65a9c7f
junsuPark <krpjs0508@gmail.com>
2023-07-27 20:49:44 +0900

    ⚡️ :: MealApiService BaseApiService 사용 설정

--- core/core-network/src/main/kotlin/com/xquare/core/network/apiservice/MealApiServiceImpl.kt
+++ core/core-network/src/main/kotlin/com/xquare/core/network/apiservice/MealApiServiceImpl.kt
@@ -4,22 +4,24 @@ import com.xquare.core.network.model.MealResponse
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
+import io.ktor.http.path
 import kotlinx.datetime.LocalDate
 
 internal class MealApiServiceImpl(
     private val httpClient: HttpClient,
+    override val baseUri: String = "meals",
 ) : MealApiService {
-    override suspend fun fetchMeal(date: LocalDate): MealResponse = httpClient.get("/meals") {
-        url { parameters.append("date", date.toString()) }
+    override suspend fun fetchMeal(date: LocalDate): MealResponse = httpClient.get {
+        url { path(baseUri, date.toString()) }
     }.body()
 
-    override suspend fun fetchMealOfMonth(date: LocalDate): List<MealResponse> =
-        httpClient.get("/meals") {
-            url {
-                parameters.run {
-                    append("year", date.year.toString())
-                    append("month", date.monthNumber.toString())
-                }
+    override suspend fun fetchMealOfMonth(date: LocalDate): List<MealResponse> = httpClient.get {
+        url {
+            path(baseUri)
+            parameters.run {
+                append("year", date.year.toString())
+                append("month", date.monthNumber.toString())
             }
-        }.body()
+        }
+    }.body()
 }
