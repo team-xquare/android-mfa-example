b577577c4dd01d906fa7a08d339275b0de95c876
junsuPark <krpjs0508@gmail.com>
2023-07-27 15:53:47 +0900

    ⚡️ :: Token 주입 로직 구현

--- core/core-datastore/src/main/kotlin/com/xquare/core/datastore/di/DataStoreModule.kt
+++ core/core-datastore/src/main/kotlin/com/xquare/core/datastore/di/DataStoreModule.kt
@@ -1,9 +1,11 @@
 package com.xquare.core.datastore.di
 
+import com.xquare.core.common.di.DiQualifier
 import com.xquare.core.datastore.datasource.UserPreferencesDataSource
 import com.xquare.core.datastore.datasource.UserPreferencesDataSourceImpl
 import com.xquare.core.datastore.preferencesDatastore
 import org.koin.android.ext.koin.androidApplication
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val dataStoreModule = module {
@@ -11,6 +13,18 @@ val dataStoreModule = module {
         datasourceModule,
     )
     single { androidApplication().preferencesDatastore }
+    factory(named(DiQualifier.Authorization.ACCESS_TOKEN)) {
+        get<UserPreferencesDataSource>().fetchAccessToken()
+    }
+    factory(named(DiQualifier.Authorization.ACCESS_TOKEN_EXPIRATION)) {
+        get<UserPreferencesDataSource>().fetchAccessTokenExpiration()
+    }
+    factory(named(DiQualifier.Authorization.REFRESH_TOKEN)) {
+        get<UserPreferencesDataSource>().fetchRefreshToken()
+    }
+    factory(named(DiQualifier.Authorization.REFRESH_TOKEN_EXPIRATION)) {
+        get<UserPreferencesDataSource>().fetchRefreshTokenExpiration()
+    }
 }
 
 private val datasourceModule = module {
