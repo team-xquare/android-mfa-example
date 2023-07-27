4dec6a27fd2c8bc64796573039afa271b5053632
junsuPark <krpjs0508@gmail.com>
2023-07-27 16:01:03 +0900

    ♻️ :: AuthorizationUtils 접근 제한자 수정

--- core/core-common/src/main/kotlin/com/xquare/core/common/util/AuthorizationUtils.kt
+++ core/core-common/src/main/kotlin/com/xquare/core/common/util/AuthorizationUtils.kt
@@ -4,25 +4,25 @@ import com.xquare.core.common.di.DiQualifier
 import org.koin.core.qualifier.named
 import org.koin.java.KoinJavaComponent.get
 
-internal val accessToken: String
+val accessToken: String
     inline get() = get(
         clazz = String::class.java,
         qualifier = named(DiQualifier.Authorization.ACCESS_TOKEN),
     )
 
-internal val accessTokenExpiration: String
+val accessTokenExpiration: String
     inline get() = get(
         clazz = String::class.java,
         qualifier = named(DiQualifier.Authorization.ACCESS_TOKEN_EXPIRATION),
     )
 
-internal val refreshToken: String
+val refreshToken: String
     inline get() = get(
         clazz = String::class.java,
         qualifier = named(DiQualifier.Authorization.REFRESH_TOKEN),
     )
 
-internal val refreshTokenExpiration: String
+val refreshTokenExpiration: String
     inline get() = get(
         clazz = String::class.java,
         qualifier = named(DiQualifier.Authorization.REFRESH_TOKEN_EXPIRATION),
