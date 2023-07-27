cea002c1969fcdc3d985e7cc542dc5449372fbef
junsuPark <krpjs0508@gmail.com>
2023-07-27 15:59:10 +0900

    ⚡️ :: koin network 모듈 수정

--- core/core-network/src/main/kotlin/com/xquare/core/network/di/NetworkModule.kt
+++ core/core-network/src/main/kotlin/com/xquare/core/network/di/NetworkModule.kt
@@ -1,13 +1,24 @@
 package com.xquare.core.network.di
 
+import com.xquare.core.network.apiservice.MealApiService
+import com.xquare.core.network.apiservice.MealApiServiceImpl
+import com.xquare.core.network.client.httpClient
 import com.xquare.core.network.datasource.MealNetworkDataSource
 import com.xquare.core.network.datasource.MealNetworkDataSourceImpl
 import org.koin.dsl.module
 
 val networkModule = module {
-    includes(dataSourceModule)
+    includes(
+        dataSourceModule,
+        apiServiceModule,
+    )
+    single { httpClient }
 }
 
 private val dataSourceModule = module {
-    single<MealNetworkDataSource> { MealNetworkDataSourceImpl() }
+    single<MealNetworkDataSource> { MealNetworkDataSourceImpl(get()) }
+}
+
+private val apiServiceModule = module {
+    single<MealApiService> { MealApiServiceImpl(get()) }
 }
