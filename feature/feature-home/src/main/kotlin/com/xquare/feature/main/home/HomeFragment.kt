47f7a9466d6a7cfa94d886bb40564100b79cef32
junsuPark <krpjs0508@gmail.com>
2023-07-28 10:54:05 +0900

    ⚡️ :: HomeFragment에 layout 파라미터 전달

--- feature/feature-home/src/main/kotlin/com/xquare/feature/main/home/HomeFragment.kt
+++ feature/feature-home/src/main/kotlin/com/xquare/feature/main/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.xquare.feature.main.home
 
 import androidx.fragment.app.Fragment
+import com.xquare.feature.home.R
 
 // todo make base fragment component
-class HomeFragment : Fragment()
+class HomeFragment : Fragment(R.layout.fragment_home)
