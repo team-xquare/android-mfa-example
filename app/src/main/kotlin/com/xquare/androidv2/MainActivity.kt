bda07060cdba99f55fd99b3a8b574ae81c53e887
junsuPark <krpjs0508@gmail.com>
2023-07-30 18:12:49 +0900

    ⚡️ :: status bars 투명처리

--- app/src/main/kotlin/com/xquare/androidv2/MainActivity.kt
+++ app/src/main/kotlin/com/xquare/androidv2/MainActivity.kt
@@ -3,6 +3,7 @@ package com.xquare.androidv2
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
 import com.xquare.androidv2.databinding.ActivityMainBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -13,5 +14,6 @@ internal class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
     }
 }
