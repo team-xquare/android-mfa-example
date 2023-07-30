a1aa730fc90d534f1da2cfb799d10e6529bace65
junsuPark <krpjs0508@gmail.com>
2023-07-30 18:05:50 +0900

    ⚡️ :: main activity binding 설정

--- app/src/main/kotlin/com/xquare/androidv2/MainActivity.kt
+++ app/src/main/kotlin/com/xquare/androidv2/MainActivity.kt
@@ -1,8 +1,17 @@
 package com.xquare.androidv2
 
+import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
+import com.xquare.androidv2.databinding.ActivityMainBinding
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 internal class MainActivity : AppCompatActivity() {
     private val viewModel: MainActivityViewModel by viewModel()
+    private val binding by lazy { ActivityMainBinding.inflate(LayoutInflater.from(this)) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
 }
