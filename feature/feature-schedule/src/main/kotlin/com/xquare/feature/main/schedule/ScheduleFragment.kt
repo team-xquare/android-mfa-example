a91161a12560ad77076007dc1203998f9d56a3b2
junsuPark <krpjs0508@gmail.com>
2023-08-03 22:31:07 +0900

    📑 :: schedule fragment 추가

---
+++ feature/feature-schedule/src/main/kotlin/com/xquare/feature/main/schedule/ScheduleFragment.kt
@@ -0,0 +1,6 @@
+package com.xquare.feature.main.schedule
+
+import androidx.fragment.app.Fragment
+import com.xquare.feature.schedule.R
+
+class ScheduleFragment : Fragment(R.layout.fragment_schedule)
