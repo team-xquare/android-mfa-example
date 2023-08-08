5b8b6a2286a4a6ec552268600f54cd1ff221387b
junsuPark <krpjs0508@gmail.com>
2023-08-08 15:35:26 +0900

    📝 :: TODO 추가

--- app/src/main/kotlin/com/xquare/androidv2/MainFragment.kt
+++ app/src/main/kotlin/com/xquare/androidv2/MainFragment.kt
@@ -2,4 +2,7 @@ package com.xquare.androidv2
 
 import androidx.fragment.app.Fragment
 
-internal class MainFragment : Fragment(R.layout.content_main)
+// todo make base fragment component
+internal class MainFragment : Fragment(R.layout.content_main) {
+
+}
