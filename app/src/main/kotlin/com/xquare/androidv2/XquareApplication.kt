5c7769abdc6a415081e6a46b580f9b53bf170281
junsuPark <krpjs0508@gmail.com>
2023-08-08 19:46:40 +0900

    ⚡️ :: flipper 플러그인 설정

--- app/src/main/kotlin/com/xquare/androidv2/XquareApplication.kt
+++ app/src/main/kotlin/com/xquare/androidv2/XquareApplication.kt
@@ -3,6 +3,12 @@ package com.xquare.androidv2
 import android.app.Application
 import com.facebook.flipper.android.AndroidFlipperClient
 import com.facebook.flipper.android.utils.FlipperUtils
+import com.facebook.flipper.plugins.databases.DatabasesFlipperPlugin
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.flipper.plugins.navigation.NavigationFlipperPlugin
+import com.facebook.flipper.plugins.network.NetworkFlipperPlugin
+import com.facebook.flipper.plugins.sharedpreferences.SharedPreferencesFlipperPlugin
 import com.facebook.soloader.SoLoader
 import com.google.android.material.color.DynamicColors
 import com.xquare.androidv2.di.xquareModule
@@ -41,7 +47,24 @@ internal class XquareApplication : Application() {
     private fun initFlipper() {
         if (isDebugEnabled && FlipperUtils.shouldEnableFlipper(this@XquareApplication)) {
             AndroidFlipperClient.getInstance(this@XquareApplication).apply {
-                // addPlugin()
+
+                // todo make function receives collection of plugins by parameter
+                addPlugin(DatabasesFlipperPlugin(this@XquareApplication))
+                addPlugin(
+                    InspectorFlipperPlugin(
+                        this@XquareApplication,
+                        DescriptorMapping.withDefaults(),
+                    ),
+                )
+                addPlugin(NavigationFlipperPlugin.getInstance())
+                addPlugin(NetworkFlipperPlugin())
+                addPlugin(
+                    SharedPreferencesFlipperPlugin(
+                        this@XquareApplication,
+                        // todo make const variable
+                        "preferences-datastore-xquare",
+                    ),
+                )
             }.start()
         }
     }
