2d71022722e94f8dc194242e52add0ae70cb0142
junsuPark <krpjs0508@gmail.com>
2023-08-08 15:51:24 +0900

    ♻️ :: Fragment 이름 수정

--- app/src/main/kotlin/com/xquare/androidv2/fragment/services/MainFragment.kt
+++ app/src/main/kotlin/com/xquare/androidv2/fragment/services/MainFragment.kt
@@ -6,12 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.xquare.androidv2.R
-import com.xquare.androidv2.databinding.FragmentMainBinding
+import com.xquare.androidv2.databinding.FragmentServicesMainBinding
 
 // todo make base fragment component
 internal class MainFragment : Fragment(R.layout.content_main) {
 
-    private var _binding: FragmentMainBinding
+    private var _binding: FragmentServicesMainBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
