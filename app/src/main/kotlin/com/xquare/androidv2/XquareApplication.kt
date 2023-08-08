7fb54720c97937f8a12ce774851783a82d423ba4
junsuPark <krpjs0508@gmail.com>
2023-08-08 19:27:35 +0900

    ⚡️ :: flipper application 지정

--- app/src/main/kotlin/com/xquare/androidv2/XquareApplication.kt
+++ app/src/main/kotlin/com/xquare/androidv2/XquareApplication.kt
@@ -1,6 +1,9 @@
 package com.xquare.androidv2
 
 import android.app.Application
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.android.utils.FlipperUtils
+import com.facebook.soloader.SoLoader
 import com.google.android.material.color.DynamicColors
 import com.xquare.androidv2.di.xquareModule
 import com.xquare.core.common.util.isDebugEnabled
@@ -13,6 +16,8 @@ internal class XquareApplication : Application() {
         super.onCreate()
         initKoin()
         initDynamicColors()
+        initSoLoader()
+        initFlipper()
     }
 
     private fun initKoin() {
@@ -27,5 +32,17 @@ internal class XquareApplication : Application() {
 
     private fun initDynamicColors() {
         DynamicColors.applyToActivitiesIfAvailable(this@XquareApplication)
+    }
+
+    private fun initSoLoader() {
+        SoLoader.init(this@XquareApplication, false)
+    }
+
+    private fun initFlipper() {
+        if (isDebugEnabled && FlipperUtils.shouldEnableFlipper(this@XquareApplication)) {
+            AndroidFlipperClient.getInstance(this@XquareApplication).apply {
+                // addPlugin()
+            }.start()
+        }
     }
 }
