38816985293a4180a28087ce25eb651b6969818d
junsuPark <krpjs0508@gmail.com>
2023-08-10 16:29:01 +0900

    ⚡️ :: navigation bars height 함수 추가

--- core/core-ui/src/main/kotlin/com/xquare/core/ui/util/WindowInsetsUtils.kt
+++ core/core-ui/src/main/kotlin/com/xquare/core/ui/util/WindowInsetsUtils.kt
@@ -1,6 +1,18 @@
 package com.xquare.core.ui.util
 
+import android.annotation.SuppressLint
+import android.content.Context
+
 data class SystemBarsHeight(
     val statusBarHeight: Int,
     val navigationBarHeight: Int,
 )
+
+@SuppressLint("InternalInsetResource", "DiscouragedApi")
+fun measureNavigationBarHeight(context: Context): Int {
+    val resourceId = context.resources.getIdentifier( "navigation_bar_height", "dimen", "android")
+
+    check(resourceId >= 0)
+
+    return context.resources.getDimensionPixelSize(resourceId)
+}
