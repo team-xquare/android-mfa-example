7d3d98917f1333fafc674fa50bc2ed0fd2a85e6c
junsuPark <krpjs0508@gmail.com>
2023-08-10 19:33:44 +0900

    ⚡️ :: bottom navigation 설정

--- app/src/main/kotlin/com/xquare/androidv2/fragment/services/MainFragment.kt
+++ app/src/main/kotlin/com/xquare/androidv2/fragment/services/MainFragment.kt
@@ -5,10 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.xquare.androidv2.R
 import com.xquare.androidv2.databinding.FragmentServicesMainBinding
 import com.xquare.core.ui.util.measuredSystemNavigationBarHeight
-import com.xquare.core.ui.util.measuredSystemStatusBarHeight
 
 // todo make base fragment component
 internal class MainFragment : Fragment(R.layout.fragment_services_main) {
@@ -17,11 +19,13 @@ internal class MainFragment : Fragment(R.layout.fragment_services_main) {
     private val binding: FragmentServicesMainBinding
         get() = _binding!!
 
-    private val navHostFragment: NavHostFragment
-        get() = childFragmentManager.findFragmentById(R.id.nav_host_fragment_content_services_main) as NavHostFragment
-
     private val systemNavigationHeight: Int
         get() = context.measuredSystemNavigationBarHeight
+
+    private val navHostFragment: NavHostFragment
+        get() = childFragmentManager.findFragmentById(R.id.nav_host_fragment_content_services_main) as NavHostFragment
+    private val navController: NavController
+        get() = navHostFragment.navController
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,11 +33,18 @@ internal class MainFragment : Fragment(R.layout.fragment_services_main) {
         savedInstanceState: Bundle?,
     ): View {
         _binding = FragmentServicesMainBinding.inflate(inflater, container, false)
-
-        val navView = binding.includedLayoutServicesMain.bottomNavigationServicesMain
-        navView.setPadding(0, 0, 0, systemNavigationHeight)
-
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        initBottomNavigation()
+    }
+
+    private fun initBottomNavigation() {
+        binding.includedLayoutServicesMain.bottomNavigationServicesMain.run {
+            setPadding(0, 0, 0, systemNavigationHeight)
+            setupWithNavController(navController)
+        }
     }
 
     override fun onDestroyView() {
