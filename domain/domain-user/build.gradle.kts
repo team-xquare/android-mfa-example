543761f8855f6636fee95e5a5d4cac861f6df54c
junsuPark <krpjs0508@gmail.com>
2023-08-18 22:44:59 +0900

    📑 :: user domain 모듈 추가

---
+++ domain/domain-user/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    id(
+        Plugins.LIBRARY_ANDROID,
+        Plugins.KOTLIN_ANDROID,
+        Plugins.KAPT,
+    )
+}
+
+android {
+    namespace = "com.xquare.domain.user"
+    compileSdk = AndroidConfig.Sdk.COMPILE_SDK
+
+    defaultConfig {
+        minSdk = AndroidConfig.Sdk.MINIMUM_SDK
+        consumerProguardFiles("consumer-rules.pro")
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro",
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = AndroidConfig.CompileOptions.SOURCE_COMPATIBILITY
+        targetCompatibility = AndroidConfig.CompileOptions.TARGET_COMPATIBILITY
+    }
+    kotlinOptions { jvmTarget = AndroidConfig.KotlinOptions.JVM_TARGET }
+    buildFeatures {
+        viewBinding = true
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion =
+            AndroidConfig.ComposeOptions.VERSION_KOTLIN_COMPILER_EXTENSION
+    }
+}
+
+dependencies {
+    implementation(
+        Libraries.Android.CORE,
+        Libraries.Android.APPCOMPAT,
+        Libraries.Koin.KOIN,
+    )
+}
