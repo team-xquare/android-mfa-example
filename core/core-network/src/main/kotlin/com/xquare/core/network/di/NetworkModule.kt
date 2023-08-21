cbc3ff4ce48ae17a413f112a66c3095dac0cbca1
junsuPark <krpjs0508@gmail.com>
2023-08-21 16:32:32 +0900

    ♻️ :: network module 코드 단순화

--- core/core-network/src/main/kotlin/com/xquare/core/network/di/NetworkModule.kt
+++ core/core-network/src/main/kotlin/com/xquare/core/network/di/NetworkModule.kt
@@ -1,28 +1,8 @@
 package com.xquare.core.network.di
 
-import com.xquare.core.network.apiservice.MealApiService
-import com.xquare.core.network.apiservice.MealApiServiceImpl
 import com.xquare.core.network.client.httpClient
-import com.xquare.core.network.datasource.MealNetworkDataSource
-import com.xquare.core.network.datasource.MealNetworkDataSourceImpl
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 val networkModule: Module
-    get() = module {
-        includes(
-            dataSourceModule,
-            apiServiceModule,
-        )
-        single { httpClient }
-    }
-
-private val dataSourceModule: Module
-    get() = module {
-        single<MealNetworkDataSource> { MealNetworkDataSourceImpl(get()) }
-    }
-
-private val apiServiceModule: Module
-    get() = module {
-        single<MealApiService> { MealApiServiceImpl(get()) }
-    }
+    get() = module { single { httpClient } }
