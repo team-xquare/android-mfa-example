945f1ae8bf78b91ad8efdaf9a1339c307183a88b
junsuPark <krpjs0508@gmail.com>
2023-08-21 23:10:30 +0900

    ⚡️ :: JwtStore 구현

---
+++ core/core-jwt/src/main/kotlin/com/xquare/core/jwt/dao/JwtStore.kt
@@ -0,0 +1,22 @@
+package com.xquare.core.jwt.dao
+
+import kotlinx.datetime.LocalDateTime
+
+// todo typealias 분리
+typealias Token = String
+typealias Expiration = LocalDateTime
+
+interface JwtStore {
+    fun loadAccessToken(): Token
+    fun storeAccessToken(token: Token)
+    fun clearAccessToken()
+    fun loadAccessTokenExpiration(): Expiration
+    fun storeAccessTokenExpiration(expiration: Expiration)
+    fun clearAccessTokenExpiration()
+    fun loadRefreshToken(): Token
+    fun storeRefreshToken(token: Token)
+    fun clearRefreshToken()
+    fun loadRefreshTokenExpiration(): Expiration
+    fun storeRefreshTokenExpiration(expiration: Expiration)
+    fun clearRefreshTokenExpiration()
+}
