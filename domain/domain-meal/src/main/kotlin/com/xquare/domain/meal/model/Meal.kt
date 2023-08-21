ae4a37e167704feb84bdc3948220cf554b83e908
junsuPark <krpjs0508@gmail.com>
2023-08-21 16:05:37 +0900

    ⚡️ :: Meal 직렬화 로직 구현

--- domain/domain-meal/src/main/kotlin/com/xquare/domain/meal/model/Meal.kt
+++ domain/domain-meal/src/main/kotlin/com/xquare/domain/meal/model/Meal.kt
@@ -1,13 +1,16 @@
 package com.xquare.domain.meal.model
 
-import java.time.LocalDate
+import kotlinx.datetime.LocalDate
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
-data class Meal(
-    val date: LocalDate,
-    val breakfast: List<String>,
-    val kcalOfBreakfast: String,
-    val lunch: List<String>,
-    val kcalOfLunch: String,
-    val dinner: List<String>,
-    val kcalOfDinner: String,
+@Serializable
+internal data class Meal(
+    @SerialName("date") val date: LocalDate,
+    @SerialName("breakfast") val breakfast: List<String>,
+    @SerialName("breakfast_kcal") val kcalOfBreakfast: String,
+    @SerialName("lunch") val lunch: List<String>,
+    @SerialName("lunch_kcal") val kcalOfLunch: String,
+    @SerialName("dinner") val dinner: List<String>,
+    @SerialName("dinner_kcal") val kcalOfDinner: String,
 )
