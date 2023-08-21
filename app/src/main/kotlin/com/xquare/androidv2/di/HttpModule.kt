d3fa15b9d2b4c89265f7f63bb5015a181736d4e7
junsuPark <krpjs0508@gmail.com>
2023-08-21 19:39:07 +0900

    ♻️ :: app di 로직 변경

--- app/src/main/kotlin/com/xquare/androidv2/di/HttpModule.kt
+++ app/src/main/kotlin/com/xquare/androidv2/di/HttpModule.kt
@@ -1,8 +1,8 @@
 package com.xquare.androidv2.di
 
 import com.xquare.androidv2.BuildConfig
-import com.xquare.core.common.di.DiQualifier
-import com.xquare.core.common.util.isDebugEnabled
+import com.xquare.common.di.DiQualifier
+import com.xquare.common.project.isDebugEnabled
 import org.koin.core.module.Module
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
--- app/src/main/kotlin/com/xquare/androidv2/di/XquareModule.kt
+++ app/src/main/kotlin/com/xquare/androidv2/di/XquareModule.kt
@@ -2,8 +2,7 @@ package com.xquare.androidv2.di
 
 import com.xquare.androidv2.BuildConfig
 import com.xquare.androidv2.RootViewModel
-import com.xquare.core.common.di.DiQualifier
-import com.xquare.core.common.di.commonModule
+import com.xquare.common.di.DiQualifier
 import com.xquare.core.database.di.databaseModule
 import com.xquare.core.datastore.di.dataStoreModule
 import com.xquare.core.network.di.networkModule
@@ -29,7 +28,6 @@ internal val xquareModule = module {
 private val coreModule: Module
     get() = module {
         includes(
-            commonModule,
             databaseModule,
             dataStoreModule,
             networkModule,
