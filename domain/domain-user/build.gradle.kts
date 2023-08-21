81e5df595af7e9fe3ab196ec7ac91e35713efcaf
junsuPark <krpjs0508@gmail.com>
2023-08-21 19:57:31 +0900

    ⚙️ :: user domain 라이브러리 의존성 추가

--- domain/domain-user/build.gradle.kts
+++ domain/domain-user/build.gradle.kts
@@ -42,6 +42,13 @@ dependencies {
     implementation(
         Libraries.Android.CORE,
         Libraries.Android.APPCOMPAT,
+        Libraries.Ktor.KTOR,
         Libraries.Koin.KOIN,
+        Libraries.Datastore.DATASTORE_PREFERENCES,
+        Libraries.Kotlin.DATETIME,
+        Libraries.Kotlin.SERIALIZATION_JSON,
     )
+    implementation(project(":core:core-database"))
+    implementation(project(":core:core-datastore"))
+    implementation(project(":core:core-network"))
 }
