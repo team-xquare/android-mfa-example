984206582bcafa738394e690950b991f1713ed9f
junsuPark <krpjs0508@gmail.com>
2023-08-21 23:22:01 +0900

    ♻️ :: jwt store 함수 시그니처 변경

--- core/core-jwt/src/main/kotlin/com/xquare/core/jwt/store/JwtStore.kt
+++ core/core-jwt/src/main/kotlin/com/xquare/core/jwt/store/JwtStore.kt
@@ -1,5 +1,6 @@
 package com.xquare.core.jwt.store
 
+import kotlinx.coroutines.flow.Flow
 import kotlinx.datetime.LocalDateTime
 
 // todo typealias 분리
@@ -7,16 +8,16 @@ typealias Token = String
 typealias Expiration = LocalDateTime
 
 interface JwtStore {
-    fun loadAccessToken(): Token
+    fun loadAccessToken(): Flow<Token>
     fun storeAccessToken(token: Token)
     fun clearAccessToken()
-    fun loadAccessTokenExpiration(): Expiration
+    fun loadAccessTokenExpiration(): Flow<Expiration>
     fun storeAccessTokenExpiration(expiration: Expiration)
     fun clearAccessTokenExpiration()
-    fun loadRefreshToken(): Token
+    fun loadRefreshToken(): Flow<Token>
     fun storeRefreshToken(token: Token)
     fun clearRefreshToken()
-    fun loadRefreshTokenExpiration(): Expiration
+    fun loadRefreshTokenExpiration(): Flow<Expiration>
     fun storeRefreshTokenExpiration(expiration: Expiration)
     fun clearRefreshTokenExpiration()
 }
