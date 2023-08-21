eb9f6a33d910ca967a22c23ff30a6f07b278c9fe
junsuPark <krpjs0508@gmail.com>
2023-08-21 16:16:29 +0900

    ♻️ :: meal entity 접근제한자 수정

--- core/core-database/src/main/kotlin/com/xquare/core/database/model/MealEntity.kt
+++ core/core-database/src/main/kotlin/com/xquare/core/database/model/MealEntity.kt
@@ -3,11 +3,10 @@ package com.xquare.core.database.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.xquare.core.model.Meal
 import kotlinx.datetime.LocalDate
 
 @Entity(tableName = "tbl_meal")
-internal data class MealEntity(
+data class MealEntity(
     @PrimaryKey @ColumnInfo(name = "date") val date: LocalDate,
     @ColumnInfo(name = "breakfast") val breakfast: List<String>,
     @ColumnInfo(name = "kcal_breakfast") val kcalOfBreakfast: String,
@@ -15,24 +14,4 @@ internal data class MealEntity(
     @ColumnInfo(name = "kcal_lunch") val kcalOfLunch: String,
     @ColumnInfo(name = "dinner") val dinner: List<String>,
     @ColumnInfo(name = "kcal_dinner") val kcalOfDinner: String,
-)
-
-internal fun Meal.asDatabaseEntity() = MealEntity(
-    date = this.date,
-    breakfast = this.breakfast,
-    kcalOfBreakfast = this.kcalOfBreakfast,
-    lunch = this.lunch,
-    kcalOfLunch = this.kcalOfLunch,
-    dinner = this.dinner,
-    kcalOfDinner = this.kcalOfDinner,
-)
-
-internal fun MealEntity.asExternalModel() = Meal(
-    date = this.date,
-    breakfast = this.breakfast,
-    kcalOfBreakfast = this.kcalOfBreakfast,
-    lunch = this.lunch,
-    kcalOfLunch = this.kcalOfLunch,
-    dinner = this.dinner,
-    kcalOfDinner = this.kcalOfDinner,
 )
