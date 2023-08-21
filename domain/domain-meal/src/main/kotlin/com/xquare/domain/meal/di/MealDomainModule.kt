1a667c716930fd99bff10ace53ea17f11d059752
junsuPark <krpjs0508@gmail.com>
2023-08-21 18:00:22 +0900

    ♻️ :: meal di 수정

--- domain/domain-meal/src/main/kotlin/com/xquare/domain/meal/di/MealDomainModule.kt
+++ domain/domain-meal/src/main/kotlin/com/xquare/domain/meal/di/MealDomainModule.kt
@@ -1,5 +1,9 @@
 package com.xquare.domain.meal.di
 
+import com.xquare.domain.meal.datasource.database.MealDatabaseDataSource
+import com.xquare.domain.meal.datasource.database.MealDatabaseDataSourceImpl
+import com.xquare.domain.meal.datasource.datastore.MealDataStoreDataSource
+import com.xquare.domain.meal.datasource.datastore.MealDataStoreDataSourceImpl
 import com.xquare.domain.meal.datasource.network.MealNetworkDataSource
 import com.xquare.domain.meal.datasource.network.MealNetworkDataSourceImpl
 import com.xquare.domain.meal.datasource.network.apiservice.MealApiService
@@ -9,6 +13,8 @@ import org.koin.dsl.module
 
 val mealDomainModule: Module
     get() = module {
+        single<MealDatabaseDataSource> { MealDatabaseDataSourceImpl(get()) }
+        single<MealDataStoreDataSource> { MealDataStoreDataSourceImpl(get()) }
         single<MealNetworkDataSource> { MealNetworkDataSourceImpl(get()) }
         single<MealApiService> { MealApiServiceImpl(get()) }
     }
