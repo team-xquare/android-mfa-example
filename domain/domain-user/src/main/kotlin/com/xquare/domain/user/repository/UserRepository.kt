a0346d377c55e30b8a140eedd8d0957c366f8066
junsuPark <krpjs0508@gmail.com>
2023-08-22 18:27:18 +0900

    ⚡️ :: user repository 임시 구현

---
+++ domain/domain-user/src/main/kotlin/com/xquare/domain/user/repository/UserRepository.kt
@@ -0,0 +1,11 @@
+package com.xquare.domain.user.repository
+
+import com.xquare.domain.user.model.input.LoginInput
+import com.xquare.domain.user.model.input.RegisterInput
+import com.xquare.domain.user.model.output.AuthOutput
+
+internal interface UserRepository {
+    suspend fun register(input: RegisterInput)
+    suspend fun login(input: LoginInput): AuthOutput
+    suspend fun reissueToken(): AuthOutput
+}
---
+++ domain/domain-user/src/main/kotlin/com/xquare/domain/user/repository/UserRepositoryImpl.kt
@@ -0,0 +1,26 @@
+package com.xquare.domain.user.repository
+
+import com.xquare.domain.user.datasource.database.UserDatabaseDataSource
+import com.xquare.domain.user.datasource.datastore.UserDataStoreDataSource
+import com.xquare.domain.user.datasource.network.UserNetworkDataSource
+import com.xquare.domain.user.model.input.LoginInput
+import com.xquare.domain.user.model.input.RegisterInput
+import com.xquare.domain.user.model.output.AuthOutput
+
+internal class UserRepositoryImpl(
+    private val userDatabaseDataSource: UserDatabaseDataSource,
+    private val userDataStoreDataSource: UserDataStoreDataSource,
+    private val userNetworkDataSource: UserNetworkDataSource,
+) : UserRepository {
+    override suspend fun register(input: RegisterInput) {
+        userNetworkDataSource.register(input)
+    }
+
+    override suspend fun login(input: LoginInput): AuthOutput {
+        return userNetworkDataSource.login(input)
+    }
+
+    override suspend fun reissueToken(): AuthOutput {
+        return userNetworkDataSource.reissueToken()
+    }
+}
