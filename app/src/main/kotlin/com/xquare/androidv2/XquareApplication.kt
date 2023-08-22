ed38377cd4890e3089ae77e82d979ecededb6ef3
junsuPark <krpjs0508@gmail.com>
2023-08-22 09:27:03 +0900

    ♻️ :: isDebugEnabled import path 변경

--- app/src/main/kotlin/com/xquare/androidv2/XquareApplication.kt
+++ app/src/main/kotlin/com/xquare/androidv2/XquareApplication.kt
@@ -12,7 +12,7 @@ import com.facebook.flipper.plugins.sharedpreferences.SharedPreferencesFlipperPl
 import com.facebook.soloader.SoLoader
 import com.google.android.material.color.DynamicColors
 import com.xquare.androidv2.di.xquareModule
-import com.xquare.core.common.util.isDebugEnabled
+import com.xquare.common.project.isDebugEnabled
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
