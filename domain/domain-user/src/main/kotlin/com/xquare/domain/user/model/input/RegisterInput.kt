834522f619e8537ccbc526a1f48895391b862bb9
junsuPark <krpjs0508@gmail.com>
2023-08-22 12:54:10 +0900

    ♻️ :: register input 네이밍 변경

--- domain/domain-user/src/main/kotlin/com/xquare/domain/user/model/input/RegisterInput.kt
+++ domain/domain-user/src/main/kotlin/com/xquare/domain/user/model/input/RegisterInput.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class RegisterRequest(
+data class RegisterInput(
     @SerialName("verification_code") val verificationCode: String,
     @SerialName("account_id") val accountId: String,
     @SerialName("password") val password: String,
