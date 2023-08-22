3bfb6d4e3368c311069710d09622ba2c01b5916d
junsuPark <krpjs0508@gmail.com>
2023-08-22 18:27:33 +0900

    ⚡️ :: user network datasource 구현

--- domain/domain-user/src/main/kotlin/com/xquare/domain/user/datasource/network/UserNetworkDataSourceImpl.kt
+++ domain/domain-user/src/main/kotlin/com/xquare/domain/user/datasource/network/UserNetworkDataSourceImpl.kt
@@ -1,3 +1,22 @@
 package com.xquare.domain.user.datasource.network
 
-internal class UserNetworkDataSourceImpl : UserNetworkDataSource
+import com.xquare.domain.user.datasource.network.apiservice.UserApiService
+import com.xquare.domain.user.model.input.LoginInput
+import com.xquare.domain.user.model.input.RegisterInput
+import com.xquare.domain.user.model.output.AuthOutput
+
+internal class UserNetworkDataSourceImpl(
+    private val userApiService: UserApiService,
+) : UserNetworkDataSource {
+    override suspend fun register(input: RegisterInput) {
+        userApiService.register(input)
+    }
+
+    override suspend fun login(input: LoginInput): AuthOutput {
+        return userApiService.login(input)
+    }
+
+    override suspend fun reissueToken(): AuthOutput {
+        return userApiService.reissueToken()
+    }
+}
