c540642fb69fc0be63863390ee5c63ecb24bf0e5
junsuPark <krpjs0508@gmail.com>
2023-08-22 09:38:47 +0900

    ⚡️ :: jwt typealias 추가

---
+++ core/core-jwt/src/main/kotlin/com/xquare/core/jwt/typealias/Token.kt
@@ -0,0 +1,8 @@
+package com.xquare.core.jwt.`typealias`
+
+import kotlinx.datetime.Instant
+
+typealias AccessToken = String
+typealias AccessTokenExpiration = Instant
+typealias RefreshToken = String
+typealias RefreshTokenExpiration = Instant
