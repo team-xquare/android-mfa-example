74cd1c37925f2aae26691dda72cd1e68746c2c93
junsuPark <krpjs0508@gmail.com>
2023-08-22 13:04:28 +0900

    📑 :: auth output 추가

---
+++ domain/domain-user/src/main/kotlin/com/xquare/domain/user/model/output/AuthOutput.kt
@@ -0,0 +1,14 @@
+package com.xquare.domain.user.model.output
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class AuthOutput(
+    @SerialName("access_token") val accessToken: String,
+    @SerialName("access_token_expire_at") val accessTokenExpiration: String,
+    @SerialName("refresh_token") val refreshToken: String,
+    @SerialName("refresh_token_expire_at") val refreshTokenExpiration: String,
+    // todo converter 구현
+    @SerialName("role") val role: String,
+)
