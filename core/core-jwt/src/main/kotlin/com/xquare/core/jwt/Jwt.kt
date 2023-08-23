6cc942c6c430c7449cde091f3da3f125fa8c025f
junsuPark <krpjs0508@gmail.com>
2023-08-23 17:08:24 +0900

    📝 :: todo 추가

--- core/core-jwt/src/main/kotlin/com/xquare/core/jwt/Jwt.kt
+++ core/core-jwt/src/main/kotlin/com/xquare/core/jwt/Jwt.kt
@@ -4,6 +4,7 @@ import com.xquare.common.di.DiQualifier
 import org.koin.core.qualifier.named
 import org.koin.java.KoinJavaComponent.get
 
+// todo di 패키지 또는 모듈로 이동
 val accessToken: String
     inline get() = get(
         clazz = String::class.java,
