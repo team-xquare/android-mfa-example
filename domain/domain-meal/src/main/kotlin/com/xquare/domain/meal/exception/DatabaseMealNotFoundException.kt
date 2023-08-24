035438323265a77b191bcd501a053ae092205e3a
junsuPark <krpjs0508@gmail.com>
2023-08-24 15:56:35 +0900

    ♻️ :: db meal not found 접근제한자 추가

--- domain/domain-meal/src/main/kotlin/com/xquare/domain/meal/exception/DatabaseMealNotFoundException.kt
+++ domain/domain-meal/src/main/kotlin/com/xquare/domain/meal/exception/DatabaseMealNotFoundException.kt
@@ -1,3 +1,3 @@
 package com.xquare.domain.meal.exception
 
-class DatabaseMealNotFoundException : RuntimeException("Meal from database not found.")
+internal class DatabaseMealNotFoundException : RuntimeException("Meal from database not found.")
