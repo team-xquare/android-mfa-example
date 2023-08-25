63314c745b92aacc834ad6f684be2075c475589d
junsuPark <krpjs0508@gmail.com>
2023-08-25 22:11:01 +0900

    ⚡️ :: BaseActivity 구현

--- core/core-ui/src/main/kotlin/com/xquare/core/ui/base/BaseActivity.kt
+++ core/core-ui/src/main/kotlin/com/xquare/core/ui/base/BaseActivity.kt
@@ -1,7 +1,21 @@
 package com.xquare.core.ui.base
 
+import android.os.Bundle
+import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.viewbinding.ViewBinding
 
-abstract class BaseActivity: AppCompatActivity() {
-    
+abstract class BaseActivity<B : ViewBinding>(
+    @LayoutRes contentLayoutId: Int,
+) : AppCompatActivity(contentLayoutId) {
+
+    private val binding: B by lazy {
+        DataBindingUtil.setContentView(this@BaseActivity, contentLayoutId)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding
+    }
 }
