5c386f6b1ee333099cf04e45f39462ad808b574a
junsuPark <krpjs0508@gmail.com>
2023-08-25 22:15:44 +0900

    ⚡️ :: BaseActivity ViewModel 추가

--- core/core-ui/src/main/kotlin/com/xquare/core/ui/base/BaseActivity.kt
+++ core/core-ui/src/main/kotlin/com/xquare/core/ui/base/BaseActivity.kt
@@ -4,15 +4,19 @@ import android.os.Bundle
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModel
 import androidx.viewbinding.ViewBinding
 
-abstract class BaseActivity<B : ViewBinding>(
+abstract class BaseActivity<B : ViewBinding, VM : ViewModel>(
     @LayoutRes contentLayoutId: Int,
 ) : AppCompatActivity(contentLayoutId) {
 
-    private val binding: B by lazy {
+    protected val binding: B by lazy {
         DataBindingUtil.setContentView(this@BaseActivity, contentLayoutId)
     }
+
+    protected open val viewModel: VM
+        get() = throw NotImplementedError()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
