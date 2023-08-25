7455876567717cdd588572314c0c26b0558db399
junsuPark <krpjs0508@gmail.com>
2023-08-25 12:49:52 +0900

    ⚡️ :: domain di 구현

--- domain/domain-meal/src/main/kotlin/com/xquare/domain/meal/di/MealDomainModule.kt
+++ domain/domain-meal/src/main/kotlin/com/xquare/domain/meal/di/MealDomainModule.kt
@@ -3,8 +3,12 @@ package com.xquare.domain.meal.di
 import com.xquare.common.project.baseUrl
 import com.xquare.domain.meal.datasource.database.MealDatabaseDataSource
 import com.xquare.domain.meal.datasource.database.MealDatabaseDataSourceImpl
+import com.xquare.domain.meal.datasource.database.service.MealDatabaseService
+import com.xquare.domain.meal.datasource.database.service.MealDatabaseServiceImpl
 import com.xquare.domain.meal.datasource.datastore.MealDataStoreDataSource
 import com.xquare.domain.meal.datasource.datastore.MealDataStoreDataSourceImpl
+import com.xquare.domain.meal.datasource.datastore.service.MealDataStoreService
+import com.xquare.domain.meal.datasource.datastore.service.MealDataStoreServiceImpl
 import com.xquare.domain.meal.datasource.network.MealNetworkDataSource
 import com.xquare.domain.meal.datasource.network.MealNetworkDataSourceImpl
 import com.xquare.domain.meal.datasource.network.service.MealNetworkService
@@ -21,13 +25,17 @@ val mealDomainModule: Module
             useCaseModule,
         )
 
-        single<MealRepository> { MealRepositoryImpl(get(), get(), get()) }
+        single<MealRepository> { MealRepositoryImpl(get(), get(), get(), get(), get()) }
     }
 
 private val dataSourceModule: Module
     get() = module {
         single<MealDatabaseDataSource> { MealDatabaseDataSourceImpl(get()) }
+        single<MealDatabaseService> { MealDatabaseServiceImpl() }
+
         single<MealDataStoreDataSource> { MealDataStoreDataSourceImpl(get()) }
+        single<MealDataStoreService> { MealDataStoreServiceImpl(get()) }
+
         single<MealNetworkDataSource> { MealNetworkDataSourceImpl(get()) }
         single<MealNetworkService> {
             MealNetworkServiceImpl(
