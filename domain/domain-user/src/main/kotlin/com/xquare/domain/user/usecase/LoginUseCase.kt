0d7412d3e48f171b3210925721cc0fb87e9f4f4c
junsuPark <krpjs0508@gmail.com>
2023-08-25 16:14:00 +0900

    ♻️ :: ommited liebreak

--- domain/domain-user/src/main/kotlin/com/xquare/domain/user/usecase/LoginUseCase.kt
+++ domain/domain-user/src/main/kotlin/com/xquare/domain/user/usecase/LoginUseCase.kt
@@ -3,7 +3,9 @@ package com.xquare.domain.user.usecase
 import com.xquare.domain.user.model.input.LoginInput
 import com.xquare.domain.user.repository.UserRepository
 
-class LoginUseCase(private val userRepository: UserRepository) {
+class LoginUseCase(
+    private val userRepository: UserRepository,
+) {
     suspend operator fun invoke(input: LoginInput) {
         userRepository.login(input)
     }
