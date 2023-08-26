18a3f458deb9ae17f96f04625ff9f3db4b4054b5
junsuPark <krpjs0508@gmail.com>
2023-08-26 23:14:23 +0900

    📝 :: BaseActivity 주석 추가

--- core/core-ui/src/main/kotlin/com/xquare/core/ui/base/BaseActivity.kt
+++ core/core-ui/src/main/kotlin/com/xquare/core/ui/base/BaseActivity.kt
@@ -3,20 +3,17 @@ package com.xquare.core.ui.base
 import android.os.Bundle
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModel
 import androidx.viewbinding.ViewBinding
 
-abstract class BaseActivity<B : ViewBinding, VM : ViewModel>(
+/**
+ * referenced from [MVVMHabit](https://github.com/goldze/MVVMHabit/blob/ab2bf079815e260501e2f42b10c1b67329b1de26/mvvmhabit/src/main/java/me/goldze/mvvmhabit/base/BaseActivity.java)
+ */
+abstract class BaseActivity<B : ViewBinding>(
     @LayoutRes contentLayoutId: Int,
 ) : AppCompatActivity(contentLayoutId) {
 
-    protected val binding: B by lazy {
-        DataBindingUtil.setContentView(this@BaseActivity, contentLayoutId)
-    }
-
-    protected open val viewModel: VM
-        get() = throw NotImplementedError()
+    protected var binding: B? = null
+        private set
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
