ec4dd3669e0b8ecec898d30caef36da420ce63e0
junsuPark <krpjs0508@gmail.com>
2023-08-26 23:42:07 +0900

    ⚡️ :: BaseActivity Binding lifecycle owner 및 unbind 로직 추가

--- core/core-ui/src/main/kotlin/com/xquare/core/ui/base/BaseActivity.kt
+++ core/core-ui/src/main/kotlin/com/xquare/core/ui/base/BaseActivity.kt
@@ -3,20 +3,32 @@ package com.xquare.core.ui.base
 import android.os.Bundle
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
-import androidx.viewbinding.ViewBinding
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 
 /**
  * referenced from [MVVMHabit](https://github.com/goldze/MVVMHabit/blob/ab2bf079815e260501e2f42b10c1b67329b1de26/mvvmhabit/src/main/java/me/goldze/mvvmhabit/base/BaseActivity.java)
  */
-abstract class BaseActivity<B : ViewBinding>(
-    @LayoutRes contentLayoutId: Int,
+abstract class BaseActivity<B : ViewDataBinding>(
+    @LayoutRes private val contentLayoutId: Int,
 ) : AppCompatActivity(contentLayoutId) {
 
-    protected var binding: B? = null
+    protected lateinit var binding: B
         private set
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding
+        initViewDataBinding(savedInstanceState)
+    }
+
+    private fun initViewDataBinding(savedInstanceState: Bundle?) {
+        binding = DataBindingUtil.setContentView<B>(this@BaseActivity, contentLayoutId).apply {
+            lifecycleOwner = this@BaseActivity
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.unbind()
     }
 }
