a516af963eacd3e6d2a6bc560b1f896bdf9f2e94
junsuPark <krpjs0508@gmail.com>
2023-08-27 21:48:36 +0900

    ⚡️ :: BaseViewModelFragment 구현

--- core/core-ui/src/main/kotlin/com/xquare/core/ui/base/BaseViewModelFragment.kt
+++ core/core-ui/src/main/kotlin/com/xquare/core/ui/base/BaseViewModelFragment.kt
@@ -1,11 +1,25 @@
 package com.xquare.core.ui.base
 
+import android.os.Bundle
+import android.view.View
 import androidx.annotation.LayoutRes
 import androidx.databinding.ViewDataBinding
-import androidx.lifecycle.ViewModel
 
-abstract class BaseViewModelFragment<B : ViewDataBinding, VM : ViewModel>(
+abstract class BaseViewModelFragment<B : ViewDataBinding, VM : BaseViewModel>(
     @LayoutRes contentLayoutId: Int,
 ) : BaseFragment<B>(
     contentLayoutId = contentLayoutId,
-)
+) {
+    protected abstract val viewModelId: Int
+    protected abstract val viewModel: VM
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+        registerViewModelDataCallBack()
+    }
+
+    protected open fun registerViewModelDataCallBack() {}
+}
