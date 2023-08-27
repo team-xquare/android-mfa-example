34c634264136aff4b7bfb95d7e165be27fc7821e
junsuPark <krpjs0508@gmail.com>
2023-08-27 13:01:41 +0900

    ⚡️ :: AutoClearedValue 구현

---
+++ core/core-ui/src/main/kotlin/com/xquare/core/ui/util/AutoClearedValue.kt
@@ -0,0 +1,52 @@
+package com.xquare.core.ui.util
+
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+/**
+ * referenced from [google GithubBrowserSample](https://github.com/android/architecture-components-samples/blob/master/GithubBrowserSample/app/src/main/java/com/android/example/github/util/AutoClearedValue.kt)
+ */
+internal class AutoClearedValue<T : Any>(
+    val fragment: Fragment,
+) : ReadWriteProperty<Fragment, T> {
+
+    private var _value: T? = null
+
+    init {
+        fragment.lifecycle.addObserver(
+            object : DefaultLifecycleObserver {
+                override fun onCreate(owner: LifecycleOwner) {
+                    fragment.viewLifecycleOwnerLiveData.observe(fragment) { viewLifecycleOwner ->
+                        viewLifecycleOwner?.lifecycle?.addObserver(
+                            object : DefaultLifecycleObserver {
+                                override fun onDestroy(owner: LifecycleOwner) {
+                                    _value = null
+                                }
+                            },
+                        )
+                    }
+                }
+            },
+        )
+    }
+
+    override fun getValue(
+        thisRef: Fragment,
+        property: KProperty<*>,
+    ): T {
+        return _value ?: throw IllegalStateException(
+            "should never call auto-cleared-value get when it might not be available",
+        )
+    }
+
+    override fun setValue(
+        thisRef: Fragment,
+        property: KProperty<*>,
+        value: T,
+    ) {
+        _value = value
+    }
+}
