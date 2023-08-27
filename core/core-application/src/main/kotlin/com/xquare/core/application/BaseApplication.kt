99936592c337bff2905ab979c15cc0ecb3edc0b2
junsuPark <krpjs0508@gmail.com>
2023-08-27 23:06:42 +0900

    ⚡️ :: BaseApplication 구현

---
+++ core/core-application/src/main/kotlin/com/xquare/core/application/BaseApplication.kt
@@ -0,0 +1,44 @@
+package com.xquare.core.application
+
+import android.app.Activity
+import android.app.Application
+import android.os.Bundle
+
+abstract class BaseApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        initActivityDefaults()
+    }
+
+    private fun initActivityDefaults() {
+        registerActivityLifecycleCallbacks(
+            object : ActivityLifecycleCallbacks {
+                override fun onActivityCreated(
+                    activity: Activity,
+                    savedInstanceState: Bundle?,
+                ) {
+                }
+
+                override fun onActivityStarted(activity: Activity) {
+                }
+
+                override fun onActivityResumed(activity: Activity) {
+                }
+
+                override fun onActivityPaused(activity: Activity) {
+                }
+
+                override fun onActivityStopped(activity: Activity) {
+                }
+
+                override fun onActivitySaveInstanceState(
+                    activity: Activity,
+                    outState: Bundle,
+                ) {
+                }
+
+                override fun onActivityDestroyed(activity: Activity) {}
+            },
+        )
+    }
+}
