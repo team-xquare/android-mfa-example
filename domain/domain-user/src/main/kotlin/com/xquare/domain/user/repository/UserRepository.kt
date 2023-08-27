c1e1839bde91e6efdafeaeb5b13fbbfaba6c8c9c
junsuPark <krpjs0508@gmail.com>
2023-08-27 23:09:16 +0900

    ♻️ :: 린트 수정

--- domain/domain-user/src/main/kotlin/com/xquare/domain/user/repository/UserRepository.kt
+++ domain/domain-user/src/main/kotlin/com/xquare/domain/user/repository/UserRepository.kt
@@ -2,7 +2,6 @@ package com.xquare.domain.user.repository
 
 import com.xquare.domain.user.model.input.LoginInput
 import com.xquare.domain.user.model.input.RegisterInput
-import com.xquare.domain.user.model.output.AuthOutput
 
 interface UserRepository {
     suspend fun register(input: RegisterInput)
