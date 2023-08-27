be397ace7bf017f1198c78d31d4dff6b03447332
junsuPark <krpjs0508@gmail.com>
2023-08-27 19:29:41 +0900

    ⚡️ :: BaseViewModel 구현

--- core/core-ui/src/main/kotlin/com/xquare/core/ui/base/BaseViewModel.kt
+++ core/core-ui/src/main/kotlin/com/xquare/core/ui/base/BaseViewModel.kt
@@ -1,5 +1,13 @@
 package com.xquare.core.ui.base
 
-abstract class BaseViewModel {
+import android.app.Application
+import android.content.Context
+import androidx.lifecycle.AndroidViewModel
 
+abstract class BaseViewModel(
+    application: Application,
+) : AndroidViewModel(application) {
+
+    protected val context: Context
+        get() = getApplication<Application>().applicationContext
 }
