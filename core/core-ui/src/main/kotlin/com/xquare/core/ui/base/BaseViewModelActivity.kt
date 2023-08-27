bb84bebead9e6b46ff5212f84a913a74ac071f4a
junsuPark <krpjs0508@gmail.com>
2023-08-27 21:48:28 +0900

    ⚡️ :: BaseViewModelActivity 구현

--- core/core-ui/src/main/kotlin/com/xquare/core/ui/base/BaseViewModelActivity.kt
+++ core/core-ui/src/main/kotlin/com/xquare/core/ui/base/BaseViewModelActivity.kt
@@ -1,11 +1,26 @@
 package com.xquare.core.ui.base
 
+import android.os.Bundle
 import androidx.annotation.LayoutRes
 import androidx.databinding.ViewDataBinding
-import androidx.lifecycle.ViewModel
 
-abstract class BaseViewModelActivity<B : ViewDataBinding, VM : ViewModel>(
+abstract class BaseViewModelActivity<B : ViewDataBinding, VM : BaseViewModel>(
     @LayoutRes contentLayoutId: Int,
 ) : BaseActivity<B>(
     contentLayoutId = contentLayoutId,
-)
+) {
+    protected abstract val viewModelId: Int
+    protected abstract val viewModel: VM
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        registerViewModelDataCallBack()
+    }
+
+    override fun initViewDataBinding(savedInstanceState: Bundle?) {
+        super.initViewDataBinding(savedInstanceState)
+        binding.run { setVariable(viewModelId, viewModel) }
+    }
+
+    protected open fun registerViewModelDataCallBack() {}
+}
