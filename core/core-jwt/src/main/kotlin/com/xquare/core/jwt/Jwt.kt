caf7d274904f223a64b111793190f02c264f70ff
junsuPark <krpjs0508@gmail.com>
2023-08-29 08:06:10 +0900

    📝 :: todo 제거

--- core/core-jwt/src/main/kotlin/com/xquare/core/jwt/Jwt.kt
+++ core/core-jwt/src/main/kotlin/com/xquare/core/jwt/Jwt.kt
@@ -4,7 +4,6 @@ import com.xquare.common.di.DiQualifier
 import org.koin.core.qualifier.named
 import org.koin.java.KoinJavaComponent.get
 
-// todo di 패키지 또는 모듈로 이동
 val accessToken: String
     inline get() = get(
         clazz = String::class.java,
