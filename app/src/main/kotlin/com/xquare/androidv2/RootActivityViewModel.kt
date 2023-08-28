4e099578af3217a62bbdd18e8e01e8ffe2358433
junsuPark <krpjs0508@gmail.com>
2023-08-28 10:29:30 +0900

    ⚡️ :: RootActivityViewModel 구현

---
+++ app/src/main/kotlin/com/xquare/androidv2/RootActivityViewModel.kt
@@ -0,0 +1,8 @@
+package com.xquare.androidv2
+
+import android.app.Application
+import com.xquare.core.ui.base.BaseViewModel
+
+internal class RootActivityViewModel(
+    application: Application,
+) : BaseViewModel(application)
