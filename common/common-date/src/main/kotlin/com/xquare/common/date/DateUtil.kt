cdf1a928b2f9005b7934033e0bb838e65c93619a
junsuPark <krpjs0508@gmail.com>
2023-08-28 22:45:34 +0900

    ♻️ :: date util 불필요 함수 제거

--- common/common-date/src/main/kotlin/com/xquare/common/date/DateUtil.kt
+++ common/common-date/src/main/kotlin/com/xquare/common/date/DateUtil.kt
@@ -1,9 +1,1 @@
 package com.xquare.common.date
-
-import kotlinx.datetime.Instant
-import kotlinx.datetime.TimeZone
-import kotlinx.datetime.toInstant
-import kotlinx.datetime.toLocalDateTime
-
-fun String.toInstant(timeZone: TimeZone = TimeZone.currentSystemDefault()): Instant =
-    this.toLocalDateTime().toInstant(timeZone)
