838dcc13f4672058fca0d0f78c7cab4a6d1b386e
junsuPark <krpjs0508@gmail.com>
2023-08-28 22:48:42 +0900

    ♻️ :: datastore di module inline 키워드 추가

--- core/core-datastore/src/main/kotlin/com/xquare/core/datastore/DataStore.kt
+++ core/core-datastore/src/main/kotlin/com/xquare/core/datastore/DataStore.kt
@@ -3,4 +3,4 @@ package com.xquare.core.datastore
 import android.content.Context
 import androidx.datastore.preferences.preferencesDataStore
 
-internal val Context.preferencesDatastore by preferencesDataStore(name = "preferences-datastore-xquare")
+val Context.preferencesDatastore by preferencesDataStore(name = "preferences-datastore-xquare")
--- core/core-datastore/src/main/kotlin/com/xquare/core/datastore/di/DataStoreModule.kt
+++ core/core-datastore/src/main/kotlin/com/xquare/core/datastore/di/DataStoreModule.kt
@@ -6,6 +6,6 @@ import org.koin.core.module.Module
 import org.koin.dsl.module
 
 val dataStoreModule: Module
-    get() = module {
+    inline get() = module {
         single { androidApplication().preferencesDatastore }
     }
